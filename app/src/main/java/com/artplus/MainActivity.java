3770905f382b11d9a3325b726f74dc108a6f2088
M <owjs3901@naver.com>
2019-07-02 18:01:52 +0900

    BLE Connect

--- app/src/main/java/com/artplus/MainActivity.java
+++ app/src/main/java/com/artplus/MainActivity.java
@@ -2,59 +2,93 @@ package com.artplus;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.ParcelUuid;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.estimote.sdk.Beacon;
-import com.estimote.sdk.BeaconManager;
-import com.estimote.sdk.Region;
-import com.estimote.sdk.SystemRequirementsChecker;
+import androidx.annotation.NonNull;
+import no.nordicsemi.android.support.v18.scanner.*;
 
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity{
-	boolean isConnected;
 	TextView tvId;
-	Region region;
-	@Override protected void onCreate(Bundle savedInstanceState){
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		tvId = (TextView) findViewById(R.id.tvId);
-		BeaconManager beaconManager = new BeaconManager(this);
-		isConnected = false;
-		beaconManager.setRangingListener(new BeaconManager.RangingListener(){
+		tvId = findViewById(R.id.tvId);
+
+
+
+		BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
+		ScanSettings settings = new ScanSettings.Builder()
+				.setLegacy(false)
+				.setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+				.setReportDelay(1000)
+				.setUseHardwareBatchingIfSupported(true)
+				.build();
+		List<ScanFilter> filters = new ArrayList<>();
+		filters.add(new ScanFilter.Builder().setDeviceAddress("C8:F9:D8:D2:8D:F4").build());
+//		filters.add(new ScanFilter.Builder().setServiceUuid(mUuid).build());
+		System.out.println("뭥ㅁㅇ");
+		scanner.startScan(filters,settings,new ScanCallback(){
 			@Override
-			public void onBeaconsDiscovered(Region region, List<Beacon> list){
-				if(!list.isEmpty()){
-					Beacon nearestBeacon = list.get(0);
-					Log.d("Airport", "Nearest places: "+nearestBeacon.getRssi());
-					tvId.setText(nearestBeacon.getRssi()+"");
-					if(!isConnected && nearestBeacon.getRssi() > -70){
-						isConnected = true;
-						AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-						dialog.setTitle("알림").setMessage("비콘이 연결되었습니다.").setPositiveButton("확인", new DialogInterface.OnClickListener(){
-							@Override
-							public void onClick(DialogInterface dialog, int which){
-							}
-						}).create().show();
-					}
-					else if(isConnected && nearestBeacon.getRssi() < -70){
-						Toast.makeText(MainActivity.this, "연결이 끊어졌습니다.", Toast.LENGTH_SHORT).show();
-						isConnected = false;
+			public void onScanResult(int callbackType, @NonNull ScanResult result){
+				super.onScanResult(callbackType, result);
+				System.out.println("스캔 결과"+result.toString());
+			}
+
+			@Override
+			public void onBatchScanResults(@NonNull List<ScanResult> results){
+				super.onBatchScanResults(results);
+
+
+
+				System.out.println("스캔 결과2"+results.size());
+				for(ScanResult r:results){
+					System.out.println("=====================================");
+					System.out.println("distance"+r.getRssi());
+					System.out.println("name"+r.getDevice().getName());
+					System.out.println(r.toString());
+					System.out.println("sc"+r.getScanRecord());
+					System.out.println(r.getDevice().toString());
+
+					try{
+						System.out.println(new String(r.getScanRecord().getBytes(),"utf-8"));
+					} catch(UnsupportedEncodingException e){
+						e.printStackTrace();
 					}
 				}
+
+
+			}
+
+			@Override
+			public void onScanFailed(int errorCode){
+				super.onScanFailed(errorCode);
+				System.out.println("스캔 실패"+errorCode);
+
 			}
 		});
-		region = new Region("ranged region", UUID.fromString("74278BDA-B644-4520-8F0C-720EAF059935"), null, null);
 	}
 
 	@Override
 	protected void onResume(){
 		super.onResume();
-		SystemRequirementsChecker.checkWithDefaultDialogs(this);
-
 	}
+
+	@Override
+	protected void onDestroy(){
+
+		super.onDestroy();
+	}
+
 }
